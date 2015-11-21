986d5cac8b423b52ac2f5c03d9457f583cd0d38c
plasmashadow <plasmashadowx@gmail.com>
2015-11-22 00:07:36 +0530

    adding becode.py and its assosiated tests

--- pytorrent/bencode.py
+++ pytorrent/bencode.py
@@ -1,71 +1,134 @@
 #author: plasmashadow
 
 import six, json
+import re
 
+# http://www.bittorrent.org/beps/bep_0003.html
+# given specification
 
-class BenCoder(json.JSONEncoder):
+class BencodeException(Exception):
+    """Base class for all bencode Exceptions"""
+
+    def __init__(self, message, data):
+        self.message = message
+        self.data = data
+
+    def __repr__(self):
+        return "<BencodeException mode = %s>[%s]=>[%s]"%(self.mode, self.message, self.data)
+
+class BencodeEncodeError(BencodeException):
+    """
+    Bencode Encoding Error while converting
+    standard objects to bencoded Strings
 
     """
-      The Bencoding is done like the one
-      which is mentioned in bittorent rfc.
+    mode = "Encode"
 
-           dictionary = "d" 1*(string anytype) "e" ; non-empty dictionary
-           list       = "l" 1*anytype "e"          ; non-empty list
-           integer    = "i" signumber "e"
-           string     = number ":" <number long sequence of any CHAR>
-           anytype    = dictionary / list / integer / string
-           signumber  = "-" number / number
-           number     = 1*DIGIT
-           CHAR       = %x00-FF                    ; any 8-bit character
-           DIGIT      = "0" / "1" / "2" / "3" / "4" /
-                        "5" / "6" / "7" / "8" / "9"
+class BencodeDecodeError(BencodeException):
+    """
+    Bencode Decoding Error while converting
+    Bencoded Strings to python Objects.
     """
 
-    def encode(self, obj):
-        print obj
-        if isinstance(obj, int):
-            return "%s%s%s"%("i", str(obj), "e")
-        if isinstance(obj, str):
-            length = len(obj)
-            return "%s:%s"%(str(length), obj)
-        if isinstance(obj, list):
-            return "%s%s%s"%("l", self._parse_list(obj), "e")
-        if isinstance(obj, dict):
-            return "%s%s%s"%("d", self._parse_dict(obj), "e")
+    mode = "Decode"
+
+def _encode_int(data):
+    """
+       Encodes the int, long in to bencoded strs
+    :param data:
+    :return String: becoded string
+    """
+    if isinstance(data, (int, long)):
+        return "%s%s%s"%("i", str(data), "e")
+    else:
+        raise BencodeEncodeError("Invalid Integer", data)
+
+def _encode_string(data):
+    """
+      Encodes the str, basestring into becoded strs
+    :param data:
+    :return: String: bencoded Strings
+    """
+    if isinstance(data, (str, basestring, unicode, )):
+        return "%s:%s"%(str(len(data)), data)
+    else:
+        raise BencodeEncodeError("Invalid String", data)
+
+def _recursive_baselist_encode(data):
+    """
+      Encodes the sequence of str, int bencoded strs
+    :param data:
+    :return:
+    """
+    strs = ""
+    for element in data:
+        if isinstance(element, (str, basestring, unicode)):
+            strs += _encode_string(element)
+        elif isinstance(element, (int, long, )):
+            strs += _encode_int(element)
+    return strs
 
 
-    def _parse_list(self, lst):
-        """
-         while parsing list we need to parse
-         the contents of list in bencode as well
-        :param lst:
-        :return: string of parsed list
-        """
+def _encode_list(lst):
+    """
+     Encodes the list, set, tuple into bencoded strs
+    :param list:
+    :return: String: bencoded Strings
+    """
+    if isinstance(lst, (list, set, tuple,)):
+        lst = list(lst)
+        return "%s%s%s"%("l", _recursive_baselist_encode(lst), "e")
+    else:
+        raise BencodeEncodeError("Invalid Collection ", lst)
 
-        strs = ""
-        for key in lst:
-            strs += self.default(key)
+def _encode_dict(dct):
 
-        return strs
-
-    def _parse_dict(self, dct):
-        """
-         Trying to parse the dictionary to bencoded
-         elements.
-        :param dct:
-        :return:
-        """
-
-        strs = ""
+    """
+     Encodes the key, value pair to becoded strs
+    :param dct:
+    :return: String: bencoded strings
+    """
+    strs = ""
+    if isinstance(dct, (dict,)):
         for key, value in six.iteritems(dct):
-            strs += self.default(str(key))
-            strs += self.default(value)
-        return strs
+            if isinstance(value, (list, set, tuple)):
+                value = _encode_list(value)
+            elif isinstance(value, (str, )):
+                value = _encode_string(value)
+            elif isinstance(value, (int, )):
+                value = _encode_int(value)
+            key = _encode_string(key)
+            strs += "%s%s%s%s"%("d", key, value, "e")
+    else:
+        raise BencodeEncodeError("Invalid Dictionary", dct)
+    return strs
+
+#decoding part
+
+
+
+class Bencoder(object):
+
+    """
+      A class wrapper around bencode encoding.
+    """
 
     @classmethod
-    def bencode(cls, value):
-        return json.dumps(value, cls=cls)
+    def encode(cls, obj):
+        """encodes each python std object to bencoded strs"""
 
+        if isinstance(obj, (str, basestring, unicode)):
+            return _encode_string(obj)
+        elif isinstance(obj, (int, long, )):
+            return _encode_int(obj)
+        elif isinstance(obj, (list, tuple, set)):
+            return _encode_list(obj)
+        elif isinstance(obj, (dict,)):
+            return _encode_dict(obj)
 
+        else:
+            raise BencodeEncodeError("Object Doesn't match any base types", obj)
 
-
+    @classmethod
+    def decode(cls, becode_string):
+        pass
--- tests/test_bencode.py
+++ tests/test_bencode.py
@@ -3,10 +3,27 @@
 import unittest, json
 
 #including the target source file.
-from pytorrent.bencode import BenCoder
+from pytorrent.bencode import Bencoder
 
 class BecodeTester(unittest.TestCase):
 
-    def test_list_string_encoding(self):
-        strs = json.dumps(["announce"], cls=BenCoder)
+    def test_string_encoding(self):
+        """tests string encoding"""
+        strs = Bencoder.encode("announce")
         self.assertEqual(strs, "8:announce")
+
+    def test_integer_encoding(self):
+        """tests integer encoding"""
+        strs = Bencoder.encode(1)
+        self.assertEqual(strs, "i1e")
+
+    def test_list_encoding(self):
+        """tests list encoding"""
+        strs = Bencoder.encode([1,2])
+        self.assertEqual(strs, "li1ei2ee")
+
+    def test_dict_encoding(self):
+        """tests dictionary encoding"""
+        strs = Bencoder.encode(dict(a="1"))
+        self.assertEqual(strs, "d1:a1:1e")
+
